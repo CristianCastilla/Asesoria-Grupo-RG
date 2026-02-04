0e27a428eff77fdd84d2eac87182d83237d3f48c
Cristian Castilla <tu-email@ejemplo.com>
2026-02-04 12:54:02 +0100

    Corregir configuración de base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Asesoria-Grupo-RG/',
   server: {
     port: 3000,
   }
